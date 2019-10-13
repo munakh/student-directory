0574668a495deb8330e061206d0176db1c3293ff
EC2 Default User <ec2-user@ip-172-31-17-92.ec2.internal>
2019-10-13 19:11:19 +0000

    Modified to print short names

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -18,7 +18,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    if student[:name][0] == "H"
+    if student[:name][0] == "H" && student[:name].length < 12
       puts "#{index+=1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
