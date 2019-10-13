e2e8eaa994d2a2e8f8f48287af23cc6767919301
EC2 Default User <ec2-user@ip-172-31-17-92.ec2.internal>
2019-10-13 19:14:22 +0000

    Age and hobbies info added

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -4,7 +4,18 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    hobbies = []
+    puts "Enter age"
+    age = gets.chomp
+    while true
+      puts "Enter hobby" 
+      hobby = gets.chomp
+      if hobby != "done"
+        hobbies.push(hobby)
+      end
+    break if hobby == "done"
+    end
+    students << {name: name, cohort: :November, age: age, hobbies: hobbies}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
@@ -20,14 +31,17 @@ def print(students)
   index = 0
   while index < students.length do
     if students[index][:name][0] == "H" && students[index][:name].length < 12
-      puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
+      puts "#{index + 1}. #{students[index][:name]}" +
+      "(#{students[index][:age]})" +
+      "(Hobbies:#{students[index][:hobbies]})" +
+      "(#{students[index][:cohort]} cohort)"
     end
   index += 1
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
 students = input_students
