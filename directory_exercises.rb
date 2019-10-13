d9a4aaf94a98289b499a7a84e362de5988dc9b1a
EC2 Default User <ec2-user@ip-172-31-17-92.ec2.internal>
2019-10-13 19:12:40 +0000

    Each method modified to while loop

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -17,10 +17,12 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    if student[:name][0] == "H" && student[:name].length < 12
-      puts "#{index+=1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  index = 0
+  while index < students.length do
+    if students[index][:name][0] == "H" && students[index][:name].length < 12
+      puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
     end
+  index += 1
   end
 end
 
